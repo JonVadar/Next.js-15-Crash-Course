d03669dac7dd12a8be3f6603326c0186d99efc46
LearnWithJon <vadar@192.168.1.19>
2024-12-12 15:27:46 +0700

    update

---
+++ 10_font_optimization/src/app/layout.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: '--font-poppins',
+  weight: ["200", "400", "700"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} font-sans`}>
+        <header>
+          <nav>
+            <Link className="nav-link" href="/">
+              Home
+            </Link>
+            <div>
+              <Link className="nav-link" href="/register">
+                Register
+              </Link>
+              <Link className="nav-link" href="/dashboard">
+                Dashboard
+              </Link>
+            </div>
+          </nav>
+        </header>
+
+        <main>{children}</main>
+
+        <footer>footer</footer>
+      </body>
+    </html>
+  );
+}
